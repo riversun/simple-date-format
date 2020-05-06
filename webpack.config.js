31d4cdda27fd4fb4ad63ad94610b5b614f64a571
riversun <riversun.org@gmail.com>
2020-05-06 20:16:10 +0900

    update webpack.config for extract comment

--- webpack.config.js
+++ webpack.config.js
@@ -6,93 +6,93 @@ const webpack = require('webpack');
 
 module.exports = (env, argv) => {
 
-    const conf = {
-        mode: 'development',
-        devServer: {
-            open: true,
-            openPage: 'index.html',
-            contentBase: path.join(__dirname, 'public'),
-            watchContentBase: true,
-            port: 8080,
-            host: argv.mode === 'production' ? `localhost` : `localhost`,
-            disableHostCheck: true,
-        },
-        entry: {
-            'simple-date-format': ['./src/simple-date-format.js'],
+  const conf = {
+    mode: 'development',
+    devServer: {
+      open: true,
+      openPage: 'index.html',
+      contentBase: path.join(__dirname, 'public'),
+      watchContentBase: true,
+      port: 8080,
+      host: argv.mode === 'production' ? `localhost` : `localhost`,
+      disableHostCheck: true,
+    },
+    entry: {
+      'simple-date-format': ['./src/simple-date-format.js'],
 
+    },
+    output: {
+      path: path.join(__dirname, 'lib'),
+      publicPath: '/',
+      filename: argv.mode === 'production' ? `[name].js` : `[name].js`,  //`[name].min.js`
+      library: 'SimpleDateFormat',
+      libraryExport: 'default',
+      libraryTarget: 'umd',
+      globalObject: 'this',//for both browser and node.js
+      umdNamedDefine: true,
+      auxiliaryComment: {
+        root: 'for Root',
+        commonjs: 'for CommonJS environment',
+        commonjs2: 'for CommonJS2 environment',
+        amd: 'for AMD environment'
+      }
+    },
+
+    optimization: {
+      minimizer: [new TerserPlugin({
+        //extractComments: true,
+        //cache: true,
+        //parallel: true,
+        //sourceMap: true,
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
         },
-        output: {
-            path: path.join(__dirname, 'lib'),
-            publicPath: '/',
-            filename: argv.mode === 'production' ? `[name].js` : `[name].js`,  //`[name].min.js`
-            library: 'SimpleDateFormat',
-            libraryExport: 'default',
-            libraryTarget: 'umd',
-            globalObject  : 'this',//for both browser and node.js
-            umdNamedDefine: true,
-            auxiliaryComment: {
-                root: 'for Root',
-                commonjs: 'for CommonJS environment',
-                commonjs2: 'for CommonJS2 environment',
-                amd: 'for AMD environment'
+        extractComments: false,
+      })],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: [
+            {
+              loader: 'babel-loader',
             }
+          ],
         },
-
-        optimization: {
-            minimizer: [new TerserPlugin({
-                //extractComments: true,
-                //cache: true,
-                //parallel: true,
-                //sourceMap: true,
-                terserOptions: {
-                    compress: {
-                        drop_console: true,
-                    },
-                }
-
-            })],
+        {
+          test: /\.scss$/,
+          use: [
+            { loader: 'style-loader' },
+            { loader: 'css-loader' },
+            { loader: 'sass-loader' },
+          ]
         },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /(node_modules|bower_components)/,
-                    use: [
-                        {
-                            loader: 'babel-loader',
-                        }
-                    ],
-                },
-                {
-                    test: /\.scss$/,
-                    use: [
-                        {loader: 'style-loader'},
-                        {loader: 'css-loader'},
-                        {loader: 'sass-loader'},
-                    ]
-                },
-                {
-                    test: /\.(png|jpg|gif)$/i,
-                    use: [
-                        {loader: 'url-loader'},
-                    ]
-                },
-            ],
-
+        {
+          test: /\.(png|jpg|gif)$/i,
+          use: [
+            { loader: 'url-loader' },
+          ]
         },
-        resolve: {
-            alias: {}
-        },
-        plugins: [
-            new webpack.BannerPlugin(`[name] v${version} Copyright (c) 2019-2020 riversun.org@gmail.com`),
-        ],
+      ],
 
-    };
+    },
+    resolve: {
+      alias: {}
+    },
+    plugins: [
+      new webpack.BannerPlugin(`[name] v${version} Copyright (c) 2019-2020 https://github.com/riversun(riversun.org@gmail.com)`),
+    ],
 
-    if (argv.mode !== 'production') {
-        conf.devtool = 'inline-source-map';
-    }
+  };
 
-    return conf;
+  if (argv.mode !== 'production') {
+    conf.devtool = 'inline-source-map';
+  }
+
+  return conf;
 
 };
